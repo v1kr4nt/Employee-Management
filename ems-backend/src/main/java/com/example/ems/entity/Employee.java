cf541cf34f86417602c70215f5a086ab6b8b50dc
vikrant <notvikrant80@gmail.com>
2024-01-19 23:08:25 +0530

    no message

---
+++ ems-backend/src/main/java/com/example/ems/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    private String lastName;
+    private String email;
+}
